a9f3fe092304dc13e3b471b04ea659a69cf92f4e
Artyom Babayan <114479248+tema-git@users.noreply.github.com>
2023-04-13 15:31:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ from statsmodels.stats.proportion import proportions_ztest
 chat_id = 401141478
 
 def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-    sl = 0.09
-    zstat, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    sl = 0.01
+    zstat, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')
 
     return pval <= sl
